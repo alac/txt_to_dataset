e3d7c6255a5eff2a6f2ac4060893d2ebb694a485
alac <alanlac@gmail.com>
2023-11-07 00:12:08 -0800

    tools.merge_prompts: add an argparser

--- tools/merge_prompts.py
+++ tools/merge_prompts.py
@@ -1,6 +1,10 @@
+""" merge_prompts takes in two folders with the /group/story/chunk.json structure and merged prompts from the
+first folder with specific key/value pairs of prompts from the second folder. """
+
 import os
 import tqdm
 import json
+import argparse
 
 from library.prompt_parser import sort_keys
 
@@ -46,3 +50,33 @@ def process_chunk(merge_base_folder, merge_extract_folder, filename, merge_keys,
     if result and len(result):
         with open(output_path, 'w', encoding='utf-8') as file:
             file.writelines(result)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        usage="""merge_prompts takes in two folders with the /group/story/chunk.json structure and merges
+prompts from the first folder with specific key/value pairs of prompts from the second folder.
+
+python -m tools.merge_prompts --base_folder base --merge_folder merge --output_folder out --keys test""")
+    parser.add_argument('--base_folder', type=str, required=True, help='Input folder path. Should contain subfolders '
+                        ' containing json files. The output will have all values from these jsons by default.')
+    parser.add_argument('--merge_folder', type=str, required=True, help='Input folder path. Should contain subfolders '
+                        ' containing json files. The output will have only the --keys values from these jsons.')
+    parser.add_argument('--output_folder', type=str, required=True, help='Output folder path. Will be populated by a '
+                        ' mirrored structure as the input folder, with modified json files.')
+    parser.add_argument('--keys', nargs='+', dest='keys', required=True, help='The keys to copy over.')
+    args = parser.parse_args()
+
+    queue = []
+    for f in os.listdir(args.base_folder):
+        fp = os.path.join(args.base_folder, f)
+        if not os.path.isdir(fp):
+            continue
+        process_folder_chunk_pass(
+            fp,
+            os.path.join(args.merge_folder, f),
+            args.keys,
+            os.path.join(args.out_folder, f),
+            queue=queue)
+    for cmd in tqdm.tqdm(queue):
+        cmd()
